be5fd5c6f5dafcac89d544a7de99e02cdd461b20
DahakaLab <dahakalab@gmail.com>
2024-10-14 21:03:57 +0300

    Refactor redirect

--- src/router/hocs/withRoles.tsx
+++ src/router/hocs/withRoles.tsx
@@ -1,57 +1,46 @@
-import { useEffect } from 'react'
 import { paths } from '@router/paths';
 import { routerConfig } from '@router/routerConfig'
 import { useAppConfigStore } from '@stores/appConfig';
 import { UserTypeEnum } from '@typings/api/user';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, Navigate } from 'react-router-dom';
 
 export const withRoutes = (Component: React.FC): React.FC => () => {
   const { pathname } = useLocation();
-  const { appConfigStore, isAppConfigLoaded } = useAppConfigStore();
-  const navigate = useNavigate();
-
-  const checkRedirect = () => {
-    const currentConfig = routerConfig.find(({ path }) => path === pathname);
-    const { currentUser } = appConfigStore;
-
-    if (currentConfig?.roles == null) {
-      return;
-    }
-
-    const { roles } = currentConfig;
-
-    if (roles.includes('unauthorized') && currentUser != null) {
-      navigate(paths.main, { replace: true });
-      return;
-    }
-
-    if (currentUser == null) {
-      navigate(paths.login, { replace: true });
-      return;
-    }
-
-    if ([UserTypeEnum.OPERATOR, UserTypeEnum.CLIENT].includes(currentUser.type) && [
-      UserTypeEnum.SUPER_ADMINISTRATOR,
-      UserTypeEnum.ADMINISTRATOR,
-      UserTypeEnum.OPERATOR,
-    ].some((roleChecked) => roles.includes(roleChecked))) {
-      navigate(paths.login, { replace: true });
-      return;
-    }
-
-    if (currentUser.type === UserTypeEnum.ADMINISTRATOR && roles.includes(
-      UserTypeEnum.SUPER_ADMINISTRATOR,
-    )) {
-      navigate(paths.main, { replace: true });
-      return;
-    }
+  const { appConfigStore } = useAppConfigStore();
+  const currentConfig = routerConfig.find(({ path }) => path === pathname);
+  
+  if (currentConfig?.roles == null) {
+    return <Component />
   }
 
-  useEffect(() => {
-    if (isAppConfigLoaded) {
-      checkRedirect();
-    }
-  }, [pathname, appConfigStore.currentUser, isAppConfigLoaded]);
+  const { currentUser } = appConfigStore;
+  const { roles } = currentConfig;
+
+  if (roles.includes('unauthorized') && currentUser != null) {
+    return <Navigate to={paths.main} replace />
+  }
+
+  if (!roles.includes('unauthorized') && currentUser == null) {
+    return <Navigate to={paths.login} replace />
+  }
+
+  if (currentUser == null) {
+    return <Component />
+  }
+
+  if ([UserTypeEnum.OPERATOR, UserTypeEnum.CLIENT].includes(currentUser.type) && [
+    UserTypeEnum.SUPER_ADMINISTRATOR,
+    UserTypeEnum.ADMINISTRATOR,
+    UserTypeEnum.OPERATOR,
+  ].some((roleChecked) => roles.includes(roleChecked))) {
+    return <Navigate to={paths.login} replace />
+  }
+
+  if (currentUser.type === UserTypeEnum.ADMINISTRATOR && roles.includes(
+    UserTypeEnum.SUPER_ADMINISTRATOR,
+  )) {
+    return <Navigate to={paths.main} replace />
+  }
 
   return <Component />
 }
